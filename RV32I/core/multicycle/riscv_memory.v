c1ec987303e204900899123416c024f4ed742c0a
mngu27 <rkdalsrn27@naver.com>
2024-08-20 18:47:18 +0900

    comment

---
+++ RV32I/core/multicycle/riscv_memory.v
@@ -0,0 +1,26 @@
+
+
+
+
+
+	pipeline_memory
+	u_pipeline_memory(
+		.i_clk				(i_clk				),
+		.i_rstn				(i_rstn				),
+		.RegWriteE			(RegWriteE			),
+		.ResultSrcE			(ResultSrcE			),
+		.MemWriteE			(MemWriteE			),
+		.ALUResultE			(ALUResultE			),
+		.WriteDataE			(WriteDataE			),
+		.RdE				(RdE				),
+		.PCPlus4E			(PCPlus4E			),
+		.RegWriteM			(RegWriteM			),
+		.ResultSrcM			(ResultSrcM			),
+		.MemWriteM			(MemWriteM			),
+		.ALUResultM			(ALUResultM			),
+		.WriteDataM			(WriteDataM			),
+		.RdM				(RdM				),
+		.PCPlus4M			(PCPlus4M			)
+	);
+
+
